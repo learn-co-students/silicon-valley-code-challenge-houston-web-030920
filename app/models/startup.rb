268fb53d7700fd05e4dd8049707004398795ee99
RafaS92 <manrvaldez..92@gmail.com>
2020-03-18 10:41:31 -0500

    Done.

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,47 @@
 class Startup
+    @@all =[]
+    attr_accessor :founder
+    attr_reader :domain, :name
+    def initialize(options)
+        @name = options[:name]
+        @founder = options[:founder]
+        @domain = options[:domain]
+        @@all.push(self)
+
+
+    end
+
+    def pivot(domain,name)
+        @domain = domain
+        @name = name
+
+
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_founder(founder_name)
+        Startup.all.find do |startup|
+            if startup.founder == founder_name
+                return startup
+            end
+        end
+
+    end
+
+    def self.domains
+        domains = []
+        Startup.all.each do |startup|
+            domains.push(startup.domain)
+        end
+        return domains
+
+
+    end
+
+
+
 
 end
